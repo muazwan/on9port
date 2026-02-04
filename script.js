a7bbf4f8b8a96e8fe786fd15450336f665a45227
Mu. Azwan Jusoh <93607274+muazwan@users.noreply.github.com>
2026-02-05 00:23:34 +0800

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,8 @@ function showTab(tabId, btn) {
   buttons.forEach(b => b.classList.remove('active'));
   btn.classList.add('active');
 }
+const glow = document.querySelector('.cursor-glow');
+
+document.addEventListener('mousemove', (e) => {
+  glow.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
+});
