0eff5582e719698b2121742eaff08d3044b6604e
Mu. Azwan Jusoh <93607274+muazwan@users.noreply.github.com>
2026-02-04 23:44:44 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-
-function showTab(tabId) {
+function showTab(tabId, btn) {
   const tabs = document.querySelectorAll('.tab');
   tabs.forEach(tab => tab.style.display = 'none');
+
   document.getElementById(tabId).style.display = 'block';
+
+  const buttons = document.querySelectorAll('.tab-btn');
+  buttons.forEach(b => b.classList.remove('active'));
+  btn.classList.add('active');
 }
